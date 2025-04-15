5efb363237ea6ef0e707fbbc2a0d68e014edd0b3
roshan-raj <roshan139154@gmail.com>
2025-04-15 23:26:06 +0530

    fix: update document title in App component to 'Tactical Hacker'

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hacked</h1>
+      <h1>Tactical Hacker</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
