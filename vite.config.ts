9a8e9a5e04dca165866bd9f460741e60fda1d289
roshan-raj <roshan139154@gmail.com>
2025-04-15 23:25:27 +0530

    feat: set up folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "/tactical-hacker",
+  plugins: [react()],
+  base: '/tactical-hacker',
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, './src/api'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 800,
+    sourcemap: false,
+  },
 });
