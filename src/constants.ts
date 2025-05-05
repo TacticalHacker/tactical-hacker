42c844d137b0e2fb3001c8b2f38cfa2849bad7ad
Nikhil Amin <aminnikhil073@gmail.com>
2025-05-06 01:27:24 +0530

    fix: update API_BASE_URL to use production endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://th-backend-kko4.onrender.com';
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://th-backend-kko4.onrender.com';
+// export const API_BASE_URL = 'http://localhost:8080';
